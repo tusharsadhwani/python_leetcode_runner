1e39a94bc89abbbb6e4f08dd17d99070b063c305
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-02-09 23:06:45 +0530

    Fix solution class scoping

--- leetcode.py
+++ leetcode.py
@@ -7,36 +7,38 @@ Usage:
     pyleet remove_duplicates.py
 """
 import sys
-from typing import Any
 
 
-def pyleet() -> None:
+def pyleet() -> int:
     """CLI interface."""
     if len(sys.argv) > 1:
         filename = sys.argv[1]
     else:
         filename = sys.stdin.read()
 
-    run_leetcode_solution(filename)
+    return run_leetcode_solution(filename)
 
 
-class Solution:
-    """Dummy Solution class"""
-
-    def noop(self, *args: Any, **kwargs: Any) -> None:
-        """Dummy function"""
-        raise Exception('No Solution class found')
-
-
-def run_leetcode_solution(filename: str) -> None:
+def run_leetcode_solution(filename: str) -> int:
     """Runs the leetcode solution file"""
     try:
         with open(filename) as file:
-            exec(file.read())
+            exec(file.read())  # pylint: disable=exec-used
 
-        function_name = dir(Solution)[-1]
-        function = Solution.__dict__[function_name]
-        function(Solution())
+        if 'Solution' not in locals():
+            print(f'Error: No Solution class found in file: {filename}')
+            return 1
+
+        solution_class = locals()['Solution']
+        function_name = dir(solution_class)[-1]
+        if function_name.startswith('_'):
+            print('Error: No function found inside Solution class')
+            return 1
+
+        function = solution_class.__dict__[function_name]
+        function(solution_class())
     except FileNotFoundError:
         print(f'Error: Unable to open file: {filename}')
-        sys.exit(1)
+        return 1
+
+    return 0
