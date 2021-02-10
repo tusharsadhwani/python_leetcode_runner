4882e0558a130aec5940cbc7a36bd3980efa28f1
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-02-11 01:28:13 +0530

    Improve implementation

--- leetcode.py
+++ leetcode.py
@@ -8,37 +8,58 @@ Usage:
 """
 import sys
 
+import pytest
+
+
+def METHOD(inputs): return None
+
+
+TESTS = []
+
 
 def pyleet() -> int:
     """CLI interface."""
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
-    else:
-        filename = sys.stdin.read()
 
+    if len(sys.argv) < 2:
+        print('Error: No filename provided')
+        return 1
+
+    filename = sys.argv[1]
     return run_leetcode_solution(filename)
 
 
 def run_leetcode_solution(filename: str) -> int:
     """Runs the leetcode solution file"""
     try:
-        with open(filename) as file:
-            exec(file.read())  # pylint: disable=exec-used
-
-        if 'Solution' not in locals():
-            print(f'Error: No Solution class found in file: {filename}')
-            return 1
-
-        solution_class = locals()['Solution']
-        function_name = dir(solution_class)[-1]
-        if function_name.startswith('_'):
-            print('Error: No function found inside Solution class')
-            return 1
-
-        function = solution_class.__dict__[function_name]
-        function(solution_class())
-    except FileNotFoundError:
-        print(f'Error: Unable to open file: {filename}')
+        sys.path.append('.')
+        module_name = filename.removesuffix('.py')
+        module = __import__(module_name)
+    except ModuleNotFoundError:
+        print(f'Error: Unable to import file: {filename}')
         return 1
+
+    if not hasattr(module, 'Solution'):
+        print(f'Error: No Solution class found in file: {filename}')
+        return 1
+
+    solution_class = getattr(module, 'Solution')
+    method_names = [name for name in dir(solution_class)
+                    if not name.startswith('_')]
+
+    if len(method_names) == 0:
+        print('Error: No methods found inside Solution class')
+        return 1
+
+    if len(method_names) > 1:
+        print('Error: Make sure you only put one method inside Solution class')
+        return 1
+
+    if not hasattr(module, 'tests'):
+        print(f'Error: No tests found in file: {filename}')
+        return 1
+
+    method_name, = method_names
+    method = getattr(solution_class(), method_name)
+    method()
 
     return 0
