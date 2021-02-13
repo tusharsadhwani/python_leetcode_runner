87fd83ea3b81d9ac1b0a62fe8fef826e86956ca0
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-02-13 20:00:30 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python_leetcode_runner",
-    version="1.0.3",
+    version="1.0.4",
     author="Tushar Sadhwani",
     author_email="tushar.sadhwani000@gmail.com",
     description="A CLI app to run any given Leetcode python solution.",
